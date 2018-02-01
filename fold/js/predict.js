8e9366a02910639156c4b7345c0ca3781c37629b
Sridhar Krishnan <sridhar.krishnan@realisticgames.co.uk>
2018-02-01 13:41:34 +0000

    first commit

---
+++ fold/js/predict.js
@@ -0,0 +1,21 @@
+
+window.addEventListener('load', function() {
+
+/*
+1. Grouping 100% chance winners
+2. Grouping one half 100% winners and another half 90% winners
+3. 2 folds -> 100% + (90 - 95)%
+4. 3 folds -> 100% + 100% + (90 - 95)%
+5. 4 folds -> 100% + 100% + 100% + (90 - 95)%
+6. 5 folds -> 100% + 100% + 100% + (90 - 95)% + (80 - 90)%
+
+*/
+
+    function myFunction() 
+    { 
+        var name, age; 
+        name = document.getElementById("inputName").value;
+        age  = parseInt(document.getElementById("inputAge").value);
+        document.getElementById("displayElement").innerHTML = name +" " + age; 
+    }
+}) // window.addEventListener('load', function() {
