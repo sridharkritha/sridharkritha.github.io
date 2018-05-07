826d0f64d02d5d5e00fdd8a946015ad87deb0c5c
sridharkritha <sridharkritha@gmail.com>
2018-05-07 18:21:21 +0100

    3js sphere exercise

--- threejs/sphereCamOrientation/sphereCamOrientation.js
+++ threejs/sphereCamOrientation/sphereCamOrientation.js
@@ -1,6 +1,4 @@
 window.onload = function () {
-	// Ref: https://github.com/josdirksen/threejs-cookbook/blob/master/03-camera/03.08-rotate-camera-around-scene-y-axis.html
-
 	// Set the scene size.
 	const WIDTH = window.innerWidth;
 	const HEIGHT = window.innerHeight;
@@ -38,16 +36,13 @@ window.onload = function () {
 			// color: 0xCC0000, 
 			emissive: 0xFF0000
 		});
-	
 
 	// Set up the sphere vars
 	const RADIUS = 20;
 	const SEGMENTS = 32;
 	const RINGS = 32;
 
-	// Create a new mesh with
-	// sphere geometry - we will cover
-	// the sphereMaterial next!
+	// Create a new mesh with sphere geometry
 	const sphere = new THREE.Mesh(
 		new THREE.SphereGeometry(
 			RADIUS,
@@ -55,80 +50,69 @@ window.onload = function () {
 			RINGS),
 		sphereMaterial);
 
-	// Move the Sphere back in Z so we
-	// can see it.
+	// Move the Sphere back in Z so we can see it.
 	sphere.position.z = 1;
 
 	// Finally, add the sphere to the scene.
 	scene.add(sphere);
 
-	/*
-	// create a point light
-	const pointLight = new THREE.PointLight(0xFFFFFF);
-	// set its position
-	pointLight.position.x = 10;
-	pointLight.position.y = 50;
-	pointLight.position.z = 130;
-	// add to the scene
-	scene.add(pointLight);
-	*/
-
-	
+	// create a spot light
 	var spotLightLeft = new THREE.SpotLight(0x00FF00);
 	spotLightLeft.position.set(100, 0, 0);
 	spotLightLeft.castShadow = true;
 	scene.add(spotLightLeft);
 
-	var spotLightHelper = new THREE.SpotLightHelper( spotLightLeft );
-	scene.add( spotLightHelper );
+	// var spotLightHelper = new THREE.SpotLightHelper( spotLightLeft );
+	// scene.add( spotLightHelper );
 
 	var spotLightRight = new THREE.SpotLight(0x0000FF);
 	spotLightRight.position.set(-100, 0, 0);
 	spotLightRight.castShadow = true;
 	scene.add(spotLightRight);
 
-	var spotLightHelper2 = new THREE.SpotLightHelper( spotLightRight );
-	scene.add( spotLightHelper2 );
+	// var spotLightHelper2 = new THREE.SpotLightHelper( spotLightRight );
+	// scene.add( spotLightHelper2 );
 
-	var radius = 100, constant = 2, elapsedTime = 0;
+	var radius = 100, constant = 1, elapsedTime = 0, rmapped = 0, antiClockwise = false;
 
 	// Setup the animation loop.
 	function animate(time) {
 		// Draw!
 		renderer.render(scene, camera);
-
-		elapsedTime += 0.01;
+		
+		if(antiClockwise)
+		{
+			elapsedTime -= 0.1;
+			if(elapsedTime < 0)
+			{
+				antiClockwise = false;
+			}
+		}
+		else
+		{
+			elapsedTime += 0.1;
+			if(elapsedTime > 45)
+			{
+				antiClockwise = true;
+			}
+		}
+		
 		// Revolving the camera around the object
 		camera.position.x = sphere.position.x + radius * Math.cos( constant * elapsedTime );
 		camera.position.z = sphere.position.z + radius * Math.sin( constant * elapsedTime );
-		camera.lookAt( sphere.position );		
+		camera.lookAt( sphere.position );
 
 		// Schedule the next frame.
 		requestAnimationFrame(animate);
 
-		TWEEN.update(time);
+		var h = rmapped * 0.01 % 1;
+		var s = 0.5;
+		var l = 0.5;
+		spotLightLeft.color.setHSL ( h, s, l );
+		spotLightRight.color.setHSL ( h, s+0.7, l- 0.3 );
+		rmapped ++;
 	}
 
 	// Schedule the first frame.
 	requestAnimationFrame(animate);
-
-	// TWEEN
-
-	// h — hue value between 0.0 and 1.0 
-	// s — saturation value between 0.0 and 1.0 
-	// l — lightness value between 0.0 and 1.0
-	// var h = rmapped * 0.01 % 1;
-	var s = 0.5;
-	var l = 0.5;
-
-	var hue = { hMin: 0}; // Start at (0, 0)
-	var tween = new TWEEN.Tween(hue) // Create a new tween that modifies 'hue'.
-	.to({ hMin: 1 }, 1000) // Move to (300, 200) in 1 second.
-	.easing(TWEEN.Easing.Quadratic.Out) // Use an easing function to make the animation smooth.
-	.onUpdate(function() { // Called after tween.js updates 'hue'.
-	// Move 'box' to the position described by 'hue' with a CSS translation.
-	spotLightLeft.color.setHSL ( hue.hMin, s, l );
-	})
-	.repeat(Infinity)
-	.start(); // Start the tween immediately.
 };
