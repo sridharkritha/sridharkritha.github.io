45ac62299e5e1f44090debdcecea59cac6ce8e12
Sridhar Krishnan <sridhar.krishnan@realisticgames.co.uk>
2018-08-22 15:18:56 +0100

    added some more params

--- artworkStacker/js/logic.js
+++ artworkStacker/js/logic.js
@@ -2,6 +2,47 @@
 
 window.addEventListener('load', function() {
 
+	// Global variables
+	var canvas = null;
+	var ctx = null;
+	var img = null;
+	var isImgLoaded = false;
+
+	  // Update Button on Click
+	  document.getElementById('updateBtnId').onclick = function () {
+		  if(isImgLoaded)
+		  {
+			isImgLoaded = false;
+			// Position
+			posX = parseInt(document.getElementById("positionX").value) || 0;
+			posY = parseInt(document.getElementById("positionY").value) || 0;
+			// Scale
+			scaleX = parseFloat(document.getElementById("scaleX").value) || 1;
+			scaleY = parseFloat(document.getElementById("scaleY").value) || 1;
+			// Clip Top Position
+			clipX = parseInt(document.getElementById("ClipTopPositionX").value) || 0;
+			clipY = parseInt(document.getElementById("ClipTopPositionY").value) || 0;
+			// Resolution
+			img.width = parseInt(document.getElementById("resolutionWidth").value) || img.width;
+			img.height = parseInt(document.getElementById("resolutionHeight").value) || img.height;
+
+			// Use a canvas
+			canvas = document.getElementById("myCanvas");
+			ctx = canvas.getContext("2d");
+			// TRS
+			ctx.translate(posX, posY);
+			// ctx.rotate(30 * Math.PI/180);
+			// var scale = 0.5;
+
+			// var xCoord = img.width/2 ;
+			// var yCoord = img.height/2;
+			// ctx.setTransform(scaleX, 0, 0, scaleY, xCoord,yCoord); // sets scale and origin
+			// ctx.drawImage(img, 0, 0, img.width, img.height, clipX, clipY,img.width * scaleX, img.height * scaleY);
+			ctx.drawImage(img, clipX, clipY, img.width - clipX, img.height - clipY, 0, 0,(img.width - clipX) * scaleX, (img.height - clipY) * scaleY);
+		  }
+
+	};
+
 function handleFileSelect(evt) {
     var files = evt.target.files; // FileList object
 
@@ -14,16 +55,18 @@ function handleFileSelect(evt) {
 	  }
 
 	  // FileReader
-		var img = document.createElement("img");
+		img = document.createElement("img");
 		var reader = new FileReader();
 		reader.onload = function(e) 
 		{
 			img.src = e.target.result;
 
 			img.onload = function() {
+				isImgLoaded = true;
+/*
 				// Use a canvas
 				var canvas = document.getElementById("myCanvas");
-				var ctx = canvas.getContext("2d");
+				ctx = canvas.getContext("2d");
 				// TRS
 				ctx.translate(350,200);
 				ctx.rotate(30 * Math.PI/180);
@@ -32,6 +75,7 @@ function handleFileSelect(evt) {
 				var yCoord = img.height/2;
 				ctx.setTransform(scale, 0, 0, scale, xCoord,yCoord); // sets scale and origin
 				ctx.drawImage(img, 0, 0,img.width, img.height);
+*/
 			  };
 		};
 		reader.readAsDataURL(f);
@@ -72,4 +116,7 @@ function handleFileSelect(evt) {
   }
 
   document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+
+  
 });
