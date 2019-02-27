832050390cb8ce5202ab72374a891a9f0c831963
Sridhar Krishnan <sridhar.krishnan@realisticgames.co.uk>
2019-02-27 16:38:47 +0000

    first bet

--- matchbook/main.js
+++ matchbook/main.js
@@ -512,8 +512,37 @@
 
 	// Submit Offers
 	// Submit one or more offers i.e. your intention or willingness to have bets with other users.
-	submitOffers = function () {
-		// https://api.matchbook.com/edge/rest/v2/offers
+	submitOffers = function (callback) {
+		var luckyBet = {
+			"odds-type":"DECIMAL",
+			"exchange-type":"back-lay",
+			"offers":
+			  [{
+				  "runner-id":1052216604020016,
+				  "side":"back",
+				  "odds": 2.4,
+				  "stake": 0.0
+			  }
+		  ]};
+
+		var options = getDefaultOptions();
+		options.method = 'POST';
+		options.url = 'https://api.matchbook.com/edge/rest/v2/offers';
+		// Cookie data for maintaining the session
+		options.headers['session-token'] = sessionToken;
+		options.json = luckyBet; // Bet info
+
+		request(options, function (error, response, body) {
+
+			if (!error && response.statusCode == 200) {
+				console.log(response);
+				return callback(null, response);
+			}
+			else {
+				console.log(response.statusCode + "Error in bet placed");
+				return callback(error,null);
+			}
+		});
 	};
 
 	// Edit Offers
@@ -832,7 +861,7 @@
 											remainingTime = currentTime - pastTime;
 											remainingTime = (1000 - remainingTime) > 0 ? 1000 - remainingTime : 0;
 											setTimeout(function() {
-												run(sessionToken);
+												// run(sessionToken);
 											}.bind(this), remainingTime);
 										}
 										
@@ -853,179 +882,25 @@
 			else{
 				console.log(sessionToken); // sessionToken
 				run(sessionToken);
+
+				if(1) {
+					//££££££££££££££££££££££££££££££££££££££££££££££££££££££££££
+					// PLACE BET - CAREFULLY
+					//££££££££££££££££££££££££££££££££££££££££££££££££££££££££££
+					setTimeout(function() {
+						submitOffers(function(err, response) {
+							if(err){
+								console.log(err);
+							}
+							else{
+								console.log(response);
+							}
+						}); // submitOffers
+					}.bind(this), 2000);
+				}
 		} 
 		}); // login
-	})(); // IIF - Main entry (login)
-
-	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	//////////////////// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$/
-	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	////////////////////////////////////// Table Creation - start /////////////////////////////////////////
-// fun(JSON.parse(xhr.responseText), 'events');
-/*
-var key = 'events';
-function fun(obj, explore)
-{
-	var targetObj = null;
-	for(var prop in obj)
-	{
-		if(obj.hasOwnProperty(prop) && prop === explore && !!obj[prop] && typeof(obj[prop])=="object")
-		{
-			targetObj = obj[prop];
-			break;
-		}
-	}
-
-	if(targetObj)
-	{
-		var keys = Object.keys(targetObj);
-		var tableContent = [];
-
-		for(var i = 0; i < keys.length; ++i)
-		{
-			var arrObjs = [];
-			var tableName = keys[i];
-			for(var p in targetObj[keys[i]])
-			{
-				if(targetObj[keys[i]].hasOwnProperty(p))
-				{
-					var x = targetObj[keys[i]][p];
-					x.name = p;
-					arrObjs.push(x);
-				}
-			}
-
-			// generateDynamicTable(arrObjs, tableName);
-			tableContent.push({"tableName": keys[i], "arrayOfObj": arrObjs});
-		}
-
-		generateDynamicTable(tableContent);
-	}
-}
-	 
-	function generateDynamicTable(tableContent){
-
-		// CREATE DYNAMIC TABLE.
-		var table = document.createElement("table");
-		table.style.width = '50%';
-		table.setAttribute('border', '1');
-		table.setAttribute('cellspacing', '0');
-		table.setAttribute('cellpadding', '5');
-
-		for(var count = 0; count < tableContent.length; ++count)
-		{
-			tableName = tableContent[count].tableName;
-			myContacts = tableContent[count].arrayOfObj;
-			var noOfContacts = myContacts.length;
-			
-			if(noOfContacts>0) {	 
-				var col = []; // define an empty array
-				for (var i = 0; i < noOfContacts; i++) {
-					for (var key in myContacts[i]) {
-						if (col.indexOf(key) === -1) {
-							col.push(key);
-						}
-					}
-				}
-				
-				// CREATE TABLE HEAD .
-				var tHead = document.createElement("thead");
-
-				/////////////////////////////////////////////////////////
-				var titleRow = document.createElement("tr");
-				var th = document.createElement("th");
-				th.innerHTML = tableName;
-				titleRow.appendChild(th);
-				tHead.appendChild(titleRow);
-				table.appendChild(tHead);
-
-				//////////////////////////////////////////////////////
-					
-				
-				// CREATE ROW FOR TABLE HEAD .
-				var hRow = document.createElement("tr");
-				// ADD COLUMN HEADER TO ROW OF TABLE HEAD.
-				for (var i = 0; i < col.length; i++) {
-						var th = document.createElement("th");
-						th.innerHTML = col[i];
-						hRow.appendChild(th);
-				}
-				tHead.appendChild(hRow);
-				table.appendChild(tHead);
-				
-				// CREATE TABLE BODY .
-				var tBody = document.createElement("tbody");
-				
-				// ADD COLUMN HEADER TO ROW OF TABLE HEAD.
-				for (var i = 0; i < noOfContacts; i++) {
-				
-						var bRow = document.createElement("tr"); // CREATE ROW FOR EACH RECORD .
-						
-						for (var j = 0; j < col.length; j++) {
-							var td = document.createElement("td");
-							td.innerHTML = myContacts[i][col[j]];
-							bRow.appendChild(td);
-						}
-						tBody.appendChild(bRow);
-				}
-				table.appendChild(tBody);
-
-				// document.body.appendChild(table);
-			}	
-		}
-		// FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-		var divContainer = document.getElementById("liveData");
-		divContainer.innerHTML = "";
-		divContainer.appendChild(table);
-	}
-*/
-
-/*
-//////////////////////////////////////// Ajax - start //////////////////////////////////////////////////////////////////
-var xhr = null;
-	
-getXmlHttpRequestObject = function()
-{
-	if(!xhr)
-	{
-		// All modern browsers have a built-in XMLHttpRequest object to request data from a server
-		// Create a new XMLHttpRequest object 
-		xhr = new XMLHttpRequest();
-	}
-	return xhr;
-};
-
-updateLiveData = function()
-{
-	var now = new Date();
-	// Date string is appended as a query with live data for not to use the cached version 
-	var url = 'result.json?' + now.getTime();
-	xhr = getXmlHttpRequestObject();
-	xhr.onreadystatechange = evenHandler;
-	// asynchronous requests
-	xhr.open("GET", url, true);
-	// Send the request over the network
-	xhr.send(null);
-};
-
-updateLiveData();
-
-function evenHandler()
-{
-	// Check response is ready or not
-	if(xhr.readyState == 4 && xhr.status == 200)
-	{
-		// dataDiv = document.getElementById('liveData');
-		// Set current data text
-		// dataDiv.innerHTML = xhr.responseText;
-		fun(JSON.parse(xhr.responseText), key);
-		// Update the live data every 1 sec
-		setTimeout(updateLiveData(), 2000);
-	}
-}
-
-//////////////////////////////////////// Ajax - end //////////////////////////////////////////////////////////////////
-*/
+	})(); // IIF - Main entry (login)	
 }()); // namespace
 
 /*
