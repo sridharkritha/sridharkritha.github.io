60aa279fd4d74c4782ebfcd2ac839ecf9297f1d5
sridharkritha <sridharkritha@gmail.com>
2020-01-19 15:41:50 +0000

    Update

--- examplejs/test/test.js
+++ examplejs/test/test.js
@@ -1,1 +1,6 @@
-console.log(Object.getPrototypeOf(Function.prototype) === Object.prototype);
+// creates an empty object without a prototype
+var obj = Object.create(null); // obj.prototype
+obj.__proto__ = 'sridhar';
+console.log(obj.__proto__); // sridhar
+console.log(obj); // Error (no toString)
+
--- spell/js/animation.js
+++ spell/js/animation.js
@@ -5,11 +5,19 @@ window.addEventListener('load', function () {
 
 	// Number Pad
 	document.getElementById('speak').onclick = function () {
+		var wordCount = wordList.length;
+		var clearIntervalHandle = null;
 
-		var qStr = "Jay is going to do show and tell tomorrow.";
+		clearInterval(clearIntervalHandle)
+		clearIntervalHandle = setInterval(() => {
+			var word = wordList[Math.floor(Math.random() * wordCount)];     // 0 to wordCount - 1
 			window.speechSynthesis.cancel();
-			textToSpeech = new SpeechSynthesisUtterance(qStr);
+			textToSpeech = new SpeechSynthesisUtterance(word);
 			window.speechSynthesis.speak(textToSpeech);
+		}, 1000);
+
+		// var qStr = "Jay is going to do show and tell tomorrow.";
+
 	};
 
 
