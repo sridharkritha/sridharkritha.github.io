5dc61c406a856e979a6319321aa874ad670b1881
Sridhar Krishnan <sridhar.krishnan@realisticgames.co.uk>
2021-08-01 06:29:03 +0100

    MemoryQAtest

---
+++ memoryQAtest/js/quest.js
@@ -0,0 +1,21 @@
+let questAns = [
+{
+    "quest": "Write Hello World Program - sridhar ?",
+    "ans": 
+`
+    int main() {
+        cout << "sridhar";
+    }
+`
+},
+
+{
+    "quest": "Write <b>Addition</b> Program - muthu ?",
+    "ans": 
+`
+    int main() {
+        cout << "muthu";
+    }
+`
+}
+]; 
---
+++ memoryQAtest/js/showHide.js
@@ -0,0 +1,51 @@
+window.addEventListener('load', function () {
+	// Button class
+	class HideShowButton {
+		constructor(divName, btnName) {
+			this.btn = document.createElement("BUTTON");    // Create a <button> element
+			this.btn.innerHTML = btnName || "Show / Hide";  // Insert text
+			this.btn.divName = divName;
+			divName.style.display = "none"; // hide the solution at the beginning
+			document.body.insertBefore(this.btn, divName);  // (newNode, sp2)
+		}
+	}
+
+	addClickEvent = function(obj) {
+		obj.btn.onclick = function () {
+			var x = this.divName; // document.getElementById("sridhar");
+			if (x.style.display === "none") {
+				x.style.display = "block";
+				this.innerHTML = "Hide Solution";
+			} else {
+				x.style.display = "none";
+				this.innerHTML = "Show Solution";
+			}
+		};
+	};
+
+	// Generate question and answers
+	/*
+		<p id="quest"></p>
+		<textarea class="textAreaAutoSizing" placeholder="Code here.... !!!" spellcheck="false"></textarea>
+		<p id="ans"></p>
+	*/
+	let noOfQuest = questAns.length || 0;
+	for(let i = 0; i < noOfQuest; ++i)
+	{
+		// Question
+		let questElmt = document.createElement("P");
+		questElmt.innerHTML = questAns[i].quest;    // Create a <div> element
+		document.body.appendChild(questElmt);
+		// Text Area
+		let txtAreaElmt = document.createElement("TEXTAREA");
+		txtAreaElmt.classList = "textAreaAutoSizing";
+		txtAreaElmt.placeholder = "Code here.... !!!";
+		txtAreaElmt.spellcheck = "false";
+		document.body.appendChild(txtAreaElmt);
+		// Solution
+		let ansElmt = document.createElement("P");
+		ansElmt.innerHTML = "<pre><code class='language-cpp hljs'>" + hljs.highlight(questAns[i].ans, {language: 'cpp'}).value + "</code></pre>"; 
+		document.body.appendChild(ansElmt);
+		addClickEvent(new HideShowButton(ansElmt, 'Show Solution'));
+	}
+});
