219f8c0ad4f248041662ab5f6adfc48489a11a11
Sridhar Krishnan <sridhar.krishnan@realisticgames.co.uk>
2021-08-01 07:00:48 +0100

    UPdate

--- memoryQAtest/js/showHide.js
+++ memoryQAtest/js/showHide.js
@@ -2,9 +2,10 @@ window.addEventListener('load', function () {
 	// Button class
 	class HideShowButton {
 		constructor(divName, btnName) {
+			// Tie the button and div together
 			this.btn = document.createElement("BUTTON");    // Create a <button> element
 			this.btn.innerHTML = btnName || "Show / Hide";  // Insert text
-			this.btn.divName = divName;
+			this.btn.divName   = divName;
 			divName.style.display = "none"; // hide the solution at the beginning
 			document.body.insertBefore(this.btn, divName);  // (newNode, sp2)
 		}
@@ -15,12 +16,34 @@ window.addEventListener('load', function () {
 			var x = this.divName; // document.getElementById("sridhar");
 			if (x.style.display === "none") {
 				x.style.display = "block";
-				this.innerHTML = "Hide Solution";
+				this.innerHTML  = "Hide Solution";
 			} else {
 				x.style.display = "none";
-				this.innerHTML = "Show Solution";
+				this.innerHTML  = "Show Solution";
 			}
 		};
+	};
+
+	// text area auto sizing
+	txtAreaOnkeydown = function (e) {
+		// Allow tab space inside the text area instead of move to next element
+		if (e.key == 'Tab') {
+			e.preventDefault();
+			var start = this.selectionStart;
+			var end   = this.selectionEnd;
+		
+			// set textarea value to: text before caret + tab + text after caret
+			this.value = this.value.substring(0, start) + "\t" + this.value.substring(end);		
+			// put caret at right position again
+			this.selectionStart =  this.selectionEnd = start + 1;
+		}
+
+		// Auto size the text area height
+		var el = this;
+		setTimeout(() => {
+			el.style.cssText = 'height:auto; padding:0';
+			el.style.cssText = 'height:' + (el.scrollHeight + (el.scrollHeight/8)) + 'px';
+		},0);
 	};
 
 	// Generate question and answers
@@ -41,6 +64,7 @@ window.addEventListener('load', function () {
 		txtAreaElmt.classList = "textAreaAutoSizing";
 		txtAreaElmt.placeholder = "Code here.... !!!";
 		txtAreaElmt.spellcheck = "false";
+		txtAreaElmt.onkeydown = txtAreaOnkeydown;
 		document.body.appendChild(txtAreaElmt);
 		// Solution
 		let ansElmt = document.createElement("P");
@@ -48,14 +72,4 @@ window.addEventListener('load', function () {
 		document.body.appendChild(ansElmt);
 		addClickEvent(new HideShowButton(ansElmt, 'Show Solution'));
 	}
-
-	// text area auto sizing
-	let textareaAutoSize = document.querySelector('textarea');
-	textareaAutoSize.onkeydown = function () {
-		var el = this;
-		setTimeout(function(){
-		  el.style.cssText = 'height:auto; padding:0';
-		  el.style.cssText = 'height:' + el.scrollHeight + 'px';
-		},0);
-	};
 });
