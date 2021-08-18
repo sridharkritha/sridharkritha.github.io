659876fde88d156f15fe041d9f3d2f8437c3a198
Sridhar Krishnan <sridhar.krishnan@realisticgames.co.uk>
2021-08-18 15:20:21 +0100

    update

--- nodejs/tutorials-nodejs/realTimeMongoDB/client.js
+++ nodejs/tutorials-nodejs/realTimeMongoDB/client.js
@@ -1,4 +1,31 @@
+
+window.addEventListener('load', function () { // needed only for the Method 1.
+
+    // Method 1: Using HTML - Load "client.html" (do NOT run "node client.js")
+    // It uses 'io' from the distributed version of socket.io from "client-dist/socket.io.js"
+    const socket = io("http://localhost:3456", { transports : ['websocket'] }); // internally emits "connection" event
+    
+    /*
+    // Method 2: Without using HTML - directly run "node client.js"
+    // It uses 'io' from the "socket.io-client" which is installed by "npm install socket.io-client --save"
+    const io = require("socket.io-client");
+    // First Connect to the Server on the Specific URL (HOST:PORT)
+    let socket = io("http://localhost:3000"); // internally emits "connection" event
+    */
+    
+    // socket.on => listener. socket.emit => sends event.
+    // Add listener for the event "myEvent" but NOT execute the callback
+    // Callback will be executed only after if you get the "myEvent"
+    socket.on("myEvent", (data) => {
+      console.log("Message: ", data); // gets executed only after the "myEvent" arrives.
+    });
+  
+  
+  }); // needed only for the Method 1.
+
+
 //window.addEventListener('load', function () {
+  /*
     const io = require("socket.io-client"); // without web
     const socket = io.connect("http://localhost:3456");
     socket.on("welcome", (data) => {
@@ -6,7 +33,7 @@
     });
 
    //  const socket = io("http://localhost:3100"); // web
-/*
+
     socket.on("connect", () => {
         console.log(socket.id); // "G5p5..."
       });     
--- nodejs/tutorials-nodejs/realTimeMongoDB/server.js
+++ nodejs/tutorials-nodejs/realTimeMongoDB/server.js
@@ -1,3 +1,29 @@
+const express = require("express");
+const app = express();
+const httpServer = require("http").createServer(app);
+const io = require("socket.io")(httpServer);
+//const mongoose = require('mongoose');
+const port = 3456;
+
+// Listen for a client connection 
+io.on("connection", (socket) => {
+    // Socket is a Link to the Client 
+    console.log("New Client is Connected!");
+    // Here the client is connected and we can exchanged data
+    socket.emit("myEvent", "Hello and Welcome to the Server");
+});
+
+// Listen the HTTP Server 
+httpServer.listen(port, () => {
+    console.log("Server Is Running Port: " + port);
+});
+
+
+
+
+
+
+/*
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
@@ -23,7 +49,7 @@ server.listen(port, () => {
     console.log('Server listening at port %d', port);
   });
 
-/*
+
 // app.use(bodyParser.json())
 
 // collections
