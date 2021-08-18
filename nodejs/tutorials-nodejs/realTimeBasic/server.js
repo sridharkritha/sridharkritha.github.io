807dec42d8a2b4908f7d4a88fd4d2b7b4dad8070
Sridhar Krishnan <sridhar.krishnan@realisticgames.co.uk>
2021-08-18 15:06:36 +0100

    Update

--- nodejs/tutorials-nodejs/realTimeBasic/server.js
+++ nodejs/tutorials-nodejs/realTimeBasic/server.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const app = express();
-const port = 3000;
-const http = require("http").createServer();
+const httpServer = require("http").createServer(app);
 const io = require("socket.io")(http);
-//Listen for a client connection 
+const port = 3000;
+
+// Listen for a client connection 
 io.on("connection", (socket) => {
-    //Socket is a Link to the Client 
+    // Socket is a Link to the Client 
     console.log("New Client is Connected!");
-    //Here the client is connected and we can exchanged 
-    //Send Message 
-    //We need to use the Socket (the link between the server and the connected user(s)).
+    // Here the client is connected and we can exchanged data
     socket.emit("myEvent", "Hello and Welcome to the Server");
 });
 
-//Listen the HTTP Server 
-http.listen(port, () => {
+// Listen the HTTP Server 
+httpServer.listen(port, () => {
     console.log("Server Is Running Port: " + port);
 });
