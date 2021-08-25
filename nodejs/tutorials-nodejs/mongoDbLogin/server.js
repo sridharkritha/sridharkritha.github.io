beffe2bbb6321b9a57a80aa46234693acd90935e
Sridhar Krishnan <sridhar.krishnan@realisticgames.co.uk>
2021-08-25 14:29:31 +0100

    update

--- nodejs/tutorials-nodejs/mongoDbLogin/server.js
+++
@@ -1,7 +0,0 @@
-// codedamn: 
-// https://www.youtube.com/watch?v=b91XgdyX-SM
-// https://github.com/mehulmpt/node-auth-youtube
-
-// https://www.bezkoder.com/node-js-mongodb-auth-jwt/
-// https://www.ctl.io/developers/blog/post/build-user-authentication-with-node-js-express-passport-and-mongodb
-// https://codebun.com/login-registration-nodejsexpress-mongodb/
